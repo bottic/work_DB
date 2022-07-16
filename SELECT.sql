9bb6cf6903cb5049dc98e96b78961c264ec915a4
bottic <99840788+bottic@users.noreply.github.com>
2022-07-16 17:42:34 +0300

    Update SELECT.sql

--- SELECT.sql
+++ SELECT.sql
@@ -1,18 +1,24 @@
+--1я задача
 SELECT name, year_off_issue FROM music_album
 WHERE year_off_issue = 2018;
 
+--2я задача
 SELECT name, duration FROM songs
 ORDER BY duration DESC
 LIMIT 1;
 
+--3я задача 
 SELECT name FROM songs
 WHERE duration >= 210;
 
+--4я задача 
 SELECT name FROM compilation
 WHERE year_off_issue BETWEEN 2018 AND 2020;
 
+--6я задача 
 SELECT name FROM songs
 WHERE name LIKE '%мой%';
 
+--5 я задача 
 SELECT name FROM singer
 WHERE name NOT LIKE '% %'
